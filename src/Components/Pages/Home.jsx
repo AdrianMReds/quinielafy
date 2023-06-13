96c66d8a3493a45d7b2bc2a278042e4cbe29eaeb
Adrián Montemayor Rojas <adrianmontemayor.r@gmail.com>
2023-06-12 22:47:09 -0600

    Basic home screen

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./home.css";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Inicio</h1>
+    <div id="home-main">
+      <div id="home-content">
+        <h1 class="home-element">Bienvenido a quinielafy</h1>
+        <h2 class="home-element">
+          El lugar donde el futbol se vive como se debe
+        </h2>
+        <NavLink exact to="/tabla">
+          <button class="home-element home-btn">Ir a tabla</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
